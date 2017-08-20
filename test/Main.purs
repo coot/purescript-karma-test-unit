84ae8b00e4e47801bc857ac416460e3fa37531d4
Marcin Szamotulski <mszamot@gmail.com>
2017-08-20 10:17:12 +0200

    add a test

--- test/Main.purs
+++ test/Main.purs
@@ -4,11 +4,12 @@ import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Aff.Console (CONSOLE)
 import Control.Monad.Eff (Eff)
 import Data.Unit (Unit)
-import Prelude (($))
-import Test.Unit (success, suite, test)
+import Prelude (discard, ($))
+import Test.Unit (failure, success, suite, test, testSkip)
 import Test.Unit.Karma (runKarma)
 
 main :: forall eff. Eff ( avar :: AVAR, console :: CONSOLE | eff) Unit
 main = runKarma do
-  suite "hello" $
-    test "world!" $ success
+  suite "runKarma test suite" $ do
+    test "run this test" $ success
+    testSkip "skip this test" $ failure "ups"
